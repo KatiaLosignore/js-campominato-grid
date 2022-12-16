9550297d679d1d64b0453541bc7eb5009c6869f0
KatiaLosignore <katialos.kl@gmail.com>
2022-12-16 19:24:23 +0100

    added bonus

--- script.js
+++ script.js
@@ -5,18 +5,35 @@ const buttonElement = document.getElementById('button');
 
 const gridElement = document.getElementById('grid');
 
+const selectInput = document.getElementById('select');
+
+
 // Numero righe e colonne
-const rows = 10;
-const cols = 10;
-const totalCells = rows * cols;
 
 
 
-buttonElement.addEventListener('click', function(){
+buttonElement.addEventListener('click', function () {
 
-    for(let i = 1; i <= totalCells; i++) {
-        
-        const cell = createCell(i);
+    gridElement.innerHTML = '';
+
+    const valueSelect = selectInput.value;
+
+
+    let totalCells;
+
+    if (valueSelect === "1") {
+        totalCells = 100;
+    } else if (valueSelect === "2") {
+        totalCells = 81;
+    } else if (valueSelect === "3") {
+        totalCells = 49;
+    }
+
+
+
+    for (let i = 1; i <= totalCells; i++) {
+
+        const cell = createCell(i, valueSelect);
 
 
         cell.addEventListener('click', () => {
@@ -33,9 +50,18 @@ buttonElement.addEventListener('click', function(){
 });
 
 
-function createCell(content) {
+function createCell(content, difficult) {
     const cell = document.createElement('div');
     cell.append(content);
     cell.classList.add('cell');
+
+    if (difficult === "1") {
+        cell.classList.add('cell-easy');
+    } else if (difficult === "2") {
+        cell.classList.add('cell-normal');
+    } else if (difficult === "3") {
+        cell.classList.add('cell-hard');
+    }
+    
     return cell;
 }
